43c59447fb8889c7f83fcfa016c56cfa5650a39c
fangxian <fangxian1108@gmail.com>
2016-05-09 23:05:53 +0200

    start doing item crawl

--- dota2max/dota2max.py
+++ dota2max/dota2max.py
@@ -10,6 +10,26 @@ from Crawler import Crawler
 def do_herocrawle(dotamax):
     dotamax.do_crawl_hero()
 
+
+def do_itemcrawle(dotamax):
+    pass
+
+
+def do_matchcrawle():
+    pass
+
+
+def data_analyse():
+    pass
+
+def data_visualization():
+    pass
+
+
 if __name__ == '__main__':
-    dotamax = Crawler("http://dotamax.com/player/hero/91698091/")
-    do_herocrawle(dotamax)
+    link = input("input the website to crawl:")
+    dotamax_hero = Crawler("http://dotamax.com/player/hero/91698091/")
+    do_herocrawle(dotamax_hero)
+    for option in ["pro","vh"]:
+        dotamax_item = Crawler("http://dotamax.com/player/items/91698091/?hero=55&skill="+option)
+        do_itemcrawle(dotamax_item)
