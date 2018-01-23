8646b7eb5ad6db5f7470baeff8e9ebfca8399459
Fangxian Yu <fangxian.yu@ericsson.com>
2018-01-23 17:03:04 +0800

    flask study

---
+++ flask_app/app/auth/views.py
@@ -0,0 +1,40 @@
+from flask import render_template, redirect, url_for, flash, request
+from . import auth
+from flask_login import login_required, login_user, logout_user
+from .forms import LoginForm, RegistrationForm
+from ..models import User
+from .. import db
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+#@app.route('/secret')
+#@login_required
+#def secret():
+#    return 'Only authenticated users are allowed!'
