6f70bbef9bd6456636be31f79a3a478f1c97adc9
Fangxian Yu <fangxian.yu@ericsson.com>
2018-01-24 13:14:44 +0800

    study

--- flask_app/app/auth/views.py
+++ flask_app/app/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, redirect, url_for, flash, request
 from . import auth
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from .forms import LoginForm, RegistrationForm
 from ..models import User
 from .. import db
+from ..email import send_email
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -31,10 +33,31 @@ def register():
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
-#@app.route('/secret')
-#@login_required
-#def secret():
-#    return 'Only authenticated users are allowed!'
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
